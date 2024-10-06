080a907e339ddd6ba30eacfed0612ff147a981af
kZ3R0k <godslayer.nevloncrates@gmail.com>
2024-10-07 02:29:00 +0530

    Delete pages directory

--- pages/Files.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-import pandas as pd
-
-upload_file = st.file_uploader("Please Upload Your File Here!")
-if upload_file:
-    df = pd.read_csv(upload_file)
-    st.session_state['df'] = df
--- pages/Tabular_columns.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-df = st.session_state['df']
-st.write(df.describe())
